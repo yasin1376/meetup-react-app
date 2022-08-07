e261de057400d4189968d66d35097f3d7932e754
Yasin <yasinm289@gmail.com>
2022-08-07 19:52:01 +0430

    added some pages

---
+++ src/pages/Favourites.js
@@ -0,0 +1,9 @@
+const Favourites = () => {
+  return (
+    <div>
+      <h1>Favourites page</h1>
+    </div>
+  );
+};
+
+export default Favourites;
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,9 @@
+const NewMeetup = () => {
+  return (
+    <div>
+      <h1>New Meetup page</h1>
+    </div>
+  );
+};
+
+export default NewMeetup;
