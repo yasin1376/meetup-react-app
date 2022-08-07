a01d7787e52305c588bbbcb93c85a2adc2006860
Yasin <yasinm289@gmail.com>
2022-08-07 19:59:51 +0430

    some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
+import NewMeetup from "./pages/NewMeetup";
+import Favourites from "./pages/Favourites";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" index element={AllMeetups} />
+        <Route path="/">
+          <AllMeetups />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetup />
+        </Route>
+        <Route path="/favourites">
+          <Favourites />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
