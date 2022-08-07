63f9ea8bc0cee7ea1675fd26cacf1a4f213f505b
Yasin <yasinm289@gmail.com>
2022-08-07 20:22:33 +0430

    router done

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
 import NewMeetup from "./pages/NewMeetup";
 import Favourites from "./pages/Favourites";
+import MainNavigation from "./components/layouts/MainNavigation";
 
 function App() {
   return (
     <BrowserRouter>
+      <MainNavigation />
       <Routes>
-        <Route path="/">
-          <AllMeetups />
-        </Route>
-        <Route path="/new-meetup">
-          <NewMeetup />
-        </Route>
-        <Route path="/favourites">
-          <Favourites />
-        </Route>
+        <Route path="/" element={<AllMeetups />} />
+        <Route path="/new-meetup" element={<NewMeetup />} />
+        <Route path="/favourites" element={<Favourites />} />
       </Routes>
     </BrowserRouter>
   );
