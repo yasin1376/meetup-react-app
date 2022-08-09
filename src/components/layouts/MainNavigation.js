aa4a9bf987af77ac0d9c4a41fa1140c358811e73
Yasin <yasinm289@gmail.com>
2022-08-09 11:38:05 +0430

    some changes

--- src/components/layouts/MainNavigation.js
+++ src/components/layouts/MainNavigation.js
@@ -13,7 +13,7 @@ const MainNavigation = () => {
           <img
             src="https://flowbite.com/docs/images/logo.svg"
             className="mr-3 h-6 sm:h-9"
-            alt="Flowbite Logo"
+            alt="Logo"
           />
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
             React Meetups
