2b2ea5a7be6f01758d5e76141589bff6f0f201c7
Yasin <yasinm289@gmail.com>
2022-08-09 12:04:49 +0430

    meetup form done

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,4 +1,8 @@
 const NewMeetup = () => {
+  const submiteMeetup = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="w-2/3 sm:w-1/2 md:w-1/2 mx-auto mt-5">
       <form>
@@ -41,6 +45,26 @@ const NewMeetup = () => {
             placeholder="Meetup Address"
           />
         </div>
+        <div class="mb-4">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+            for="description"
+          >
+            Description
+          </label>
+          <textarea
+            placeholder="Description"
+            id="description"
+            rows={5}
+            class="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          ></textarea>
+        </div>
+        <button
+          className="w-100 bg-gray-800 text-white px-3 py-2 rounded"
+          onClick={submiteMeetup}
+        >
+          Add Meetup
+        </button>
       </form>
     </div>
   );
