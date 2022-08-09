dd72ed3c27456209b85d62d97545ab7dbe1818dd
Yasin <yasinm289@gmail.com>
2022-08-09 12:14:08 +0430

    got the form data

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,54 +1,78 @@
+import { useRef } from "react";
+
 const NewMeetup = () => {
+  const titleInput = useRef();
+  const imageInput = useRef();
+  const addressInput = useRef();
+  const descriptionInput = useRef();
+
   const submiteMeetup = (e) => {
     e.preventDefault();
+    const meetupData = {
+      title: titleInput.current.value,
+      image: imageInput.current.value,
+      address: addressInput.current.value,
+      description: descriptionInput.current.value,
+    };
+
+    console.log(meetupData);
   };
 
   return (
     <div className="w-2/3 sm:w-1/2 md:w-1/2 mx-auto mt-5">
       <form>
-        <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="title">
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="title"
+          >
             Title
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="title"
             type="text"
             required
             placeholder="Title"
+            ref={titleInput}
           />
         </div>
-        <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="image">
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="image"
+          >
             Meetup Image
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="image"
             type="url"
             required
             placeholder="Meetup Image Url"
+            ref={imageInput}
           />
         </div>
-        <div class="mb-4">
+        <div className="mb-4">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
-            for="address"
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="address"
           >
             Address
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="address"
             type="text"
             required
             placeholder="Meetup Address"
+            ref={addressInput}
           />
         </div>
-        <div class="mb-4">
+        <div className="mb-4">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
-            for="description"
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="description"
           >
             Description
           </label>
@@ -56,7 +80,8 @@ const NewMeetup = () => {
             placeholder="Description"
             id="description"
             rows={5}
-            class="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            ref={descriptionInput}
+            className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           ></textarea>
         </div>
         <button
