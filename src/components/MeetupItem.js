a7d30c28e4a09d65cbe931d9b4ea033e38c92eb3
Yasin <yasinm289@gmail.com>
2022-08-12 18:28:17 +0430

    meetup done

--- src/components/MeetupItem.js
+++ src/components/MeetupItem.js
@@ -2,9 +2,17 @@ import React from "react";
 
 const MeetupItem = ({ meetup }) => {
   return (
-    <div>
-      <img src={meetup.image} alt={meetup.title} />
-      <p>{meetup.title}</p>
+    <div className="bg-gray-300 m-3 rounded">
+      <img
+        className="object-contain rounded-t"
+        src={meetup.image}
+        alt={meetup.title}
+      />
+      <div className="p-3">
+        <span className="font-bold">{meetup.title}</span>
+        <span> - {meetup.address}</span>
+        <p>{meetup.description}</p>
+      </div>
     </div>
   );
 };
