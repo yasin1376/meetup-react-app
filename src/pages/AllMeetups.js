18fa50e1bd3f327db96ff8190a0c76dea2af67ff
Yasin <yasinm289@gmail.com>
2022-08-12 17:59:02 +0430

    working on meetup list

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,4 +1,38 @@
+import { useState, useEffect } from "react";
+
 const AllMeetups = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://react-meetup-app-4e702-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setLoadedMeetups(data);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <p>Loading ...</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>All Meetups</h1>
